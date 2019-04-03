7a3d0365a4b56b03d667e5c081d7a3b9da1b2a66
Evgeniy8808 <livanov.evgeniy@gmail.com>
2019-04-03 09:23:24 +0300

    add

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,19 @@
 var gulp = require('gulp'),
+    cleanCSS = require('gulp-clean-css'),
     sass = require('gulp-sass');
 
+var config = {
+    srcDir: './sass',
+    sassPattern: '**/*.scss'
+};
+
 gulp.task('sass', function () {
-    gulp.src('./sass/**/*.scss')
+    gulp.src(config.srcDir + "/" + config.sassPattern)
         .pipe(sass().on('error', sass.logError))
+        .pipe(cleanCSS())
         .pipe(gulp.dest('./css/'));
 });
 
 gulp.task('default', function () {
-    gulp.watch('./sass/**/*.scss', ['sass']);
+    gulp.watch(config.srcDir + "/" + config.sassPattern, ['sass']);
 });
